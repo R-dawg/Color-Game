bccce2473cf37348ae6844bd6a2f056eb193f5fc
Randall <ragrate@gmail.com>
2016-10-26 23:46:34 -0400

    Pts 3 & 4. Randomize color choices and the correct color. Change colors to match correct pick.

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"];
+var colors = genRandomColor(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var header = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,10 +14,42 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.background;
 		console.log(clickedColor + " " + pickedColor);
 		if(clickedColor === pickedColor){
-			alert("CORRECT");
+			messageDisplay.textContent = "Correct";
+			changeColors();
+			header.style.background = pickedColor;
 		}else {
-			alert("WRONG");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
 }
 
+function changeColors() {
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = pickedColor;
+	}
+
+}
+
+function pickColor() {
+	var randomColor = Math.floor(Math.random() * colors.length);
+	return colors[randomColor];
+}
+
+function genRandomColor(num){
+	var colorArray = [];
+	for(var i=0; i<num; i++){
+		colorArray.push(randomColor())
+	}
+	return colorArray;
+}
+
+function randomColor() {
+	// gen red value
+	var r = Math.floor(Math.random() * 256);
+	// get green value
+	var g = Math.floor(Math.random() * 256);
+	// gen blue value
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
